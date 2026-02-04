9bc38be47d6c71750fbeaf6f4e9e54053143678b
Mariza Rocha <marizanfmrocha04@gmail.com>
2026-02-04 17:07:48 -0300

    Fix innerHTML, optimized operation functions and added Keyboard interaction

--- index.js
+++ index.js
@@ -1,159 +1,162 @@
 let tmp = 0;
 let countSignal = 0
-let result = 0
+let result = false
 const display = document.querySelector(".result")
-const number = document.querySelectorAll("#number")
+const number = document.querySelectorAll(".number")
 const calculate = document.querySelector(".calculate")
 
 number.forEach((box) => {
     box.addEventListener("click", () =>{
         display.style = "font-size: 32px;"
-        if(result === 1){
+        if(result === true){
             display.innerHTML = ""
-        }
-        if(display.innerHTML === "Only one operation allowed!"){
+            result = false
+        }else if(display.innerHTML === "Only one operation allowed!"){
             display.innerHTML = ""
+            result = false
         }
-        display.innerHTML += box.value
+        display.textContent += box.value
     })
 })
 // Operations
 document.querySelector(".sum").addEventListener("click", () =>{
-    display.innerHTML += "+"
+    display.textContent += "+"
     countSignal += 1
 })
 document.querySelector(".sub").addEventListener("click", () =>{
-    display.innerHTML += "-"
+    display.textContent += "-"
     countSignal += 1
 })
 document.querySelector(".mul").addEventListener("click", () =>{
-    display.innerHTML += "*"
+    display.textContent += "*"
     countSignal += 1
 })
 document.querySelector(".div").addEventListener("click", () =>{
-    display.innerHTML += "/"
+    display.textContent += "/"
     countSignal += 1
 })
 console.log(countSignal)
 
 calculate.addEventListener("click", () =>{
     console.log(countSignal)
-    if(display.innerHTML.includes("+")){
+    if(display.textContent.includes("+")){
         let arr = display.innerHTML.split("+")
-        tmp = sum(parseInt(arr[0]), parseInt(arr[1]))
-        display.innerHTML = tmp
-    }
-    if(display.innerHTML.includes("-")){
+        tmp = operate(parseInt(arr[0]), parseInt(arr[1]), "+")
+        display.textContent = tmp
+    }else if(display.textContent.includes("-")){
         let arr = display.innerHTML.split("-")
-        tmp = subtract(parseInt(arr[0]), parseInt(arr[1]))
-        display.innerHTML = tmp
-    }
-    if(display.innerHTML.includes("*")){
+        tmp = operate(parseInt(arr[0]), parseInt(arr[1]), "-")
+        display.textContent = tmp
+    }else if(display.textContent.includes("*")){
         let arr = display.innerHTML.split("*")
-        tmp = multiplicate(parseInt(arr[0]), parseInt(arr[1]))
-        display.innerHTML = tmp
-    }
-    if(display.innerHTML.includes("/0")){
-        display.innerHTML = "Snarky Error"
-    }
-    if(display.innerHTML.includes("/")){
+        tmp = operate(parseInt(arr[0]), parseInt(arr[1]), "*")
+        display.textContent = tmp
+    }else if(display.textContent.includes("/")){
         let arr = display.innerHTML.split("/")
-        tmp = divide(parseInt(arr[0]), parseInt(arr[1]))
-        display.innerHTML = tmp
+        tmp = operate(parseInt(arr[0]), parseInt(arr[1]), "/")
+        if(tmp % 2 === 0){
+            display.textContent = tmp
+        }else{
+            display.textContent = tmp.toFixed(2)
+        }
+
     }
-    if(countSignal > 1){
-        display.innerHTML = "Only one operation allowed!"
+    if(display.textContent.includes("/0")){
+        display.textContent = "Snarky Error"
+    }
+    if(countSignal >= 2){
+        display.textContent = "Only one operation allowed!"
         display.style = "font-size: 22px; right: 15px;\n" +
             "    top: 6px;"
     }
     countSignal = 0
-    result += 1
+    result = true
 })
 // Keyboard input
 document.addEventListener("keydown", (e) =>{
     let key = e.key
     display.style = "font-size: 32px;"
-    if(result === 1){
-        display.innerHTML = ""
+    if(result === true){
+        display.textContent = ""
+        result === false
     }
-    if(display.innerHTML === "Only one operation allowed!"){
-        display.innerHTML = ""
+    if(display.textContent === "Only one operation allowed!"){
+        display.textContent = ""
+        result === false
     }
     if(key >= 0 && key <= 9){
-        display.innerHTML += key
+        display.textContent += key
     }else if(e.code === "Minus"){
-        display.innerHTML += key
+        display.textContent += key
         countSignal += 1 // -
     }
     else if(e.code === "Digit8" && e.shiftKey === true){
-        display.innerHTML += "*"
+        display.textContent += "*"
         countSignal += 1 // *
     }else if(e.code === "Equal" && e.shiftKey === true){
-        display.innerHTML += "+"
+        display.textContent += "+"
         countSignal += 1 // +
     }else if(e.code === "Equal" && e.shiftKey === false){
-        display.innerHTML = tmp // =
+        display.textContent = tmp // =
     }else if(e.code === "Enter"){
-        display.innerHTML = tmp // Enter
+        display.textContent = tmp // Enter
     }
 })
 calculate.addEventListener("keydown", () =>{
-    if(result === 1){
-        display.innerHTML = ""
+    if(result === true){
+        display.textContent = ""
     }
-    if(display.innerHTML.includes("+")){
-        let arr = display.innerHTML.split("+")
-        tmp = sum(parseInt(arr[0]), parseInt(arr[1]))
-        display.innerHTML = tmp
-    }
-    if(display.innerHTML.includes("-")){
-        let arr = display.innerHTML.split("-")
-        tmp = subtract(parseInt(arr[0]), parseInt(arr[1]))
-        display.innerHTML = tmp
-    }
-    if(display.innerHTML.includes("*")){
-        let arr = display.innerHTML.split("*")
-        tmp = multiplicate(parseInt(arr[0]), parseInt(arr[1]))
-        display.innerHTML = tmp
-    }
-    if(display.innerHTML.includes("/0")){
-        display.innerHTML = "Snarky Error"
-    }
-    if(display.innerHTML.includes("/")){
-        let arr = display.innerHTML.split("/")
-        tmp = divide(parseInt(arr[0]), parseInt(arr[1]))
-        display.innerHTML = tmp
-    }
-    if(countSignal > 1){
-        display.innerHTML = "Only one operation allowed!"
+    if(display.textContent.includes("+")){
+        let arr = display.textContent.split("+")
+        tmp = operate(parseInt(arr[0]), parseInt(arr[1]))
+        display.textContent = tmp
+    } else if(display.textContent.includes("-")){
+        let arr = display.textContent.split("-")
+        tmp = operate(parseInt(arr[0]), parseInt(arr[1]))
+        display.textContent = tmp
+    }else if(display.textContent.includes("*")){
+        let arr = display.textContent.split("*")
+        tmp = operate(parseInt(arr[0]), parseInt(arr[1]))
+        display.textContent = tmp
+    }else if(display.textContent.includes("/0")){
+        display.textContent = "Snarky Error"
+    }else if(display.textContent.includes("/")){
+        let arr = display.textContent.split("/")
+        tmp = operate(parseInt(arr[0]), parseInt(arr[1]))
+        display.textContent = tmp
+    }else if(countSignal > 1){
+        display.textContent = "Only one operation allowed!"
         display.style = "font-size: 22px; right: 15px;\n" +
             "    top: 6px;"
     }
     countSignal = 0
-    result += 1
+    result = true
 })
-
 
 // Clear
 document.querySelector(".clear").addEventListener("click", () =>{
-    display.innerHTML = ""
-    result= 0
+    display.textContent = ""
+    result = false
     countSignal = 0
 })
+document.addEventListener("keydown", (e) =>{
+    if(result === true && e.key === "0"){
+        display.textContent = ""
+        result= false
+        countSignal = 0
+    }
+})
 // Operation Functions
-function sum(a, b){
-    return a + b
+function operate(a, b, operation){
+    if(operation === "+"){
+        return a + b
+    }else if(operation === "-"){
+        return a - b
+    }
+    else if(operation === "*"){
+        return a * b
+    }
+    else if(operation === "/"){
+        return a / b
+    }
 }
-
-function subtract(a, b){
-    return a - b
-}
-
-function multiplicate(a, b){
-    return a * b
-}
-
-function divide(a, b){
-    return a / b
-}
-
