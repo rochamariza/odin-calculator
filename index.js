1d7458032c3e729d250b520315a72b9382379b0d
Mariza Rocha <marizanfmrocha04@gmail.com>
2026-02-06 11:02:02 -0300

    Fix innerHTML, optimized operation functions and added Keyboard interaction

--- index.js
+++ index.js
@@ -1,153 +1,17 @@
 let tmp = 0;
 let countSignal = 0
+let currentOperator;
 let result = false
 const display = document.querySelector(".result")
-const number = document.querySelectorAll(".number")
+const clear = document.querySelector(".clear")
 const calculate = document.querySelector(".calculate")
+const numbers = document.querySelectorAll(".number")
+const signals = document.querySelectorAll(".signal")
 
-number.forEach((box) => {
-    box.addEventListener("click", () =>{
-        display.style = "font-size: 32px;"
-        if(result === true){
-            display.innerHTML = ""
-            result = false
-        }else if(display.innerHTML === "Only one operation allowed!"){
-            display.innerHTML = ""
-            result = false
-        }
-        display.textContent += box.value
-    })
-})
-// Operations
-document.querySelector(".sum").addEventListener("click", () =>{
-    display.textContent += "+"
-    countSignal += 1
-})
-document.querySelector(".sub").addEventListener("click", () =>{
-    display.textContent += "-"
-    countSignal += 1
-})
-document.querySelector(".mul").addEventListener("click", () =>{
-    display.textContent += "*"
-    countSignal += 1
-})
-document.querySelector(".div").addEventListener("click", () =>{
-    display.textContent += "/"
-    countSignal += 1
-})
-console.log(countSignal)
 
-calculate.addEventListener("click", () =>{
-    console.log(countSignal)
-    if(display.textContent.includes("+")){
-        let arr = display.innerHTML.split("+")
-        tmp = operate(parseInt(arr[0]), parseInt(arr[1]), "+")
-        display.textContent = tmp
-    }else if(display.textContent.includes("-")){
-        let arr = display.innerHTML.split("-")
-        tmp = operate(parseInt(arr[0]), parseInt(arr[1]), "-")
-        display.textContent = tmp
-    }else if(display.textContent.includes("*")){
-        let arr = display.innerHTML.split("*")
-        tmp = operate(parseInt(arr[0]), parseInt(arr[1]), "*")
-        display.textContent = tmp
-    }else if(display.textContent.includes("/")){
-        let arr = display.innerHTML.split("/")
-        tmp = operate(parseInt(arr[0]), parseInt(arr[1]), "/")
-        if(tmp % 2 === 0){
-            display.textContent = tmp
-        }else{
-            display.textContent = tmp.toFixed(2)
-        }
 
-    }
-    if(display.textContent.includes("/0")){
-        display.textContent = "Snarky Error"
-    }
-    if(countSignal >= 2){
-        display.textContent = "Only one operation allowed!"
-        display.style = "font-size: 22px; right: 15px;\n" +
-            "    top: 6px;"
-    }
-    countSignal = 0
-    result = true
-})
-// Keyboard input
-document.addEventListener("keydown", (e) =>{
-    let key = e.key
-    display.style = "font-size: 32px;"
-    if(result === true){
-        display.textContent = ""
-        result === false
-    }
-    if(display.textContent === "Only one operation allowed!"){
-        display.textContent = ""
-        result === false
-    }
-    if(key >= 0 && key <= 9){
-        display.textContent += key
-    }else if(e.code === "Minus"){
-        display.textContent += key
-        countSignal += 1 // -
-    }
-    else if(e.code === "Digit8" && e.shiftKey === true){
-        display.textContent += "*"
-        countSignal += 1 // *
-    }else if(e.code === "Equal" && e.shiftKey === true){
-        display.textContent += "+"
-        countSignal += 1 // +
-    }else if(e.code === "Equal" && e.shiftKey === false){
-        display.textContent = tmp // =
-    }else if(e.code === "Enter"){
-        display.textContent = tmp // Enter
-    }
-})
-calculate.addEventListener("keydown", () =>{
-    if(result === true){
-        display.textContent = ""
-    }
-    if(display.textContent.includes("+")){
-        let arr = display.textContent.split("+")
-        tmp = operate(parseInt(arr[0]), parseInt(arr[1]))
-        display.textContent = tmp
-    } else if(display.textContent.includes("-")){
-        let arr = display.textContent.split("-")
-        tmp = operate(parseInt(arr[0]), parseInt(arr[1]))
-        display.textContent = tmp
-    }else if(display.textContent.includes("*")){
-        let arr = display.textContent.split("*")
-        tmp = operate(parseInt(arr[0]), parseInt(arr[1]))
-        display.textContent = tmp
-    }else if(display.textContent.includes("/0")){
-        display.textContent = "Snarky Error"
-    }else if(display.textContent.includes("/")){
-        let arr = display.textContent.split("/")
-        tmp = operate(parseInt(arr[0]), parseInt(arr[1]))
-        display.textContent = tmp
-    }else if(countSignal > 1){
-        display.textContent = "Only one operation allowed!"
-        display.style = "font-size: 22px; right: 15px;\n" +
-            "    top: 6px;"
-    }
-    countSignal = 0
-    result = true
-})
-
-// Clear
-document.querySelector(".clear").addEventListener("click", () =>{
-    display.textContent = ""
-    result = false
-    countSignal = 0
-})
-document.addEventListener("keydown", (e) =>{
-    if(result === true && e.key === "0"){
-        display.textContent = ""
-        result= false
-        countSignal = 0
-    }
-})
 // Operation Functions
-function operate(a, b, operation){
+function operate(operation, a, b){
     if(operation === "+"){
         return a + b
     }else if(operation === "-"){
@@ -160,3 +24,50 @@ function operate(a, b, operation){
         return a / b
     }
 }
+
+function updateDisplay(text){
+    display.textContent += text
+}
+// Button click Listener
+numbers.forEach((number) => {
+    number.addEventListener("click", () =>{
+        if(result){
+            display.textContent = ""
+            result = false
+        }
+        updateDisplay(number.value)
+    })
+})
+signals.forEach((signal) => {
+    signal.addEventListener("click", () => {
+        updateDisplay(signal.value)
+        if(countSignal === 0){
+            currentOperator = signal.value
+            countSignal += 1
+        }
+    })
+})
+// Keyboard Listener
+document.addEventListener("keydown", (e) =>{
+    if(e.key >= 0 && e.key <= 9){
+        updateDisplay(e.key)
+    }else if(e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/"){
+        updateDisplay(e.key)
+    }
+})
+calculate.addEventListener("click", () =>{
+    let displayText = display.textContent
+    let arr;
+    if(displayText.includes("+") || displayText.includes("-") || displayText.includes("*") || displayText.includes("/")){
+        arr = displayText.split(currentOperator)
+        tmp = operate(currentOperator, parseInt(arr[0]), parseInt(arr[1]))
+    }
+    currentOperator = 0
+    countSignal = 0
+    result = true
+    display.textContent = tmp
+})
+// Clear
+clear.addEventListener("click", () =>{
+    display.textContent = ""
+})
