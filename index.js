8503ad801fbcbb4cf61e10e0be4b50f26ab2b43e
Mariza Rocha <marizanfmrocha04@gmail.com>
2026-02-09 17:06:15 -0300

    Fixed . signal, Added Snarky Error and Optimized the code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let tmp = 0;
+let tmp = 0
 let countSignal = 0
-let currentOperator;
+let currentOperator
 let result = false
 const display = document.querySelector(".result")
 const clear = document.querySelector(".clear")
 const calculate = document.querySelector(".calculate")
 const numbers = document.querySelectorAll(".number")
 const signals = document.querySelectorAll(".signal")
-
-
+let arr
+let value = 0
 
 // Operation Functions
 function operate(operation, a, b){
@@ -35,39 +35,84 @@ numbers.forEach((number) => {
             display.textContent = ""
             result = false
         }
-        updateDisplay(number.value)
+        if(number.value >= 0 && number.value <= 9){
+            updateDisplay(number.value)
+        }
+        else if(display.textContent !== "" && number.value === "." && value === 0){
+            updateDisplay(number.value)
+            value = 1
+            console.log(number.value)
+        }
     })
 })
 signals.forEach((signal) => {
     signal.addEventListener("click", () => {
-        updateDisplay(signal.value)
-        if(countSignal === 0){
-            currentOperator = signal.value
-            countSignal += 1
+        if(signal.value === "+" || signal.value === "-" || signal.value === "*" || signal.value === "/"){
+            if(display.textContent !== "" && countSignal === 0){
+                updateDisplay(signal.value)
+                currentOperator = signal.value
+                countSignal = 1
+                result = false
+                value = 0
+                console.log(signal.value)
+            }
         }
     })
 })
 // Keyboard Listener
 document.addEventListener("keydown", (e) =>{
     if(e.key >= 0 && e.key <= 9){
+        if(result){
+            display.textContent = ""
+            result = false
+        }
         updateDisplay(e.key)
     }else if(e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/"){
+        if(display.textContent !== "" && countSignal === 0){
+            updateDisplay(e.key)
+            currentOperator = e.key
+            countSignal = 1
+            result = false
+        }
+    }else if(display.textContent !== "" && e.key === "." && value === 0){
         updateDisplay(e.key)
+        value = 1
     }
 })
-calculate.addEventListener("click", () =>{
-    let displayText = display.textContent
-    let arr;
-    if(displayText.includes("+") || displayText.includes("-") || displayText.includes("*") || displayText.includes("/")){
-        arr = displayText.split(currentOperator)
-        tmp = operate(currentOperator, parseInt(arr[0]), parseInt(arr[1]))
+// Calculate
+document.addEventListener("keydown", (e) => {
+    if(e.key === "Enter"){
+        calculate.click()
     }
-    currentOperator = 0
-    countSignal = 0
-    result = true
-    display.textContent = tmp
+})
+
+calculate.addEventListener("click", () =>{
+    if(display.textContent.includes("+") || display.textContent.includes("-") || display.textContent.includes("*") || display.textContent.includes("/")){
+        arr = display.textContent.split(currentOperator)
+        console.log(arr)
+        if(currentOperator === "/" && parseFloat(arr[1]) === 0){
+            display.textContent = "Snarky Error"
+            result = true
+        }else{
+            tmp = operate(currentOperator, parseFloat(arr[0]), parseFloat(arr[1]))
+            result = true
+            if(tmp % 2 === 0 || tmp % 2 === 1){
+                display.textContent = tmp
+            }
+            else{
+                display.textContent = tmp.toFixed(2)
+            }
+        }
+        currentOperator = 0
+        countSignal = 0
+    }
+
 })
 // Clear
 clear.addEventListener("click", () =>{
     display.textContent = ""
+    currentOperator = 0
+    countSignal = 0
+    result = false
+    value = 0
 })
